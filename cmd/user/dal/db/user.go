39a925ba4250043107f4389a16bd9eb3103c772a
TTaket <575500603@qq.com>
2023-01-24 16:47:47 +0800

    merge: 合并分支

--- cmd/user/dal/db/user.go
+++ cmd/user/dal/db/user.go
@@ -8,6 +8,10 @@ import (
 )
 
 type User struct {
+<<<<<<< HEAD
+	gorm.Model
+=======
+>>>>>>> upstream/master
 	ID            int       `gorm:"column:id;primary_key;AUTO_INCREMENT"`
 	CreateTime    time.Time `gorm:"column:create_time;default:CURRENT_TIMESTAMP;NOT NULL"`
 	Name          string    `gorm:"column:name;unique;NOT NULL"`
@@ -34,13 +38,24 @@ func MGetUsers(ctx context.Context, userIds []int64) ([]*User, error) {
 	return res, nil
 }
 
-// CreateUser create user info.
-// 传入用户信息内容 在数据库创建用户.
-func CreateUser(ctx context.Context, users []*User) error {
-	return DB.WithContext(ctx).Create(users).Error
+// 传入用户id 返回用户信息.
+func GetUser(ctx context.Context, userID int64) (User, error) {
+	res := User{}
+
+	if err := DB.WithContext(ctx).Where("id = ?", userID).Find(&res).Error; err != nil {
+		return res, err
+	}
+	return res, nil
 }
 
-// QueryUser query list of user info.
+// 传入用户信息内容 在数据库创建用户.
+func CreateUsers(ctx context.Context, users []*User) error {
+	return DB.WithContext(ctx).Create(users).Error
+}
+func CreateUser(ctx context.Context, user *User) error {
+	return DB.WithContext(ctx).Create(user).Error
+}
+
 // 传入用户名称 查找用户信息.
 func QueryUser(ctx context.Context, userName string) ([]*User, error) {
 	res := make([]*User, 0)
