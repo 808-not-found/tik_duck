1a9ad68d01e720e2794e2bb152bfb6eff1e1b7a0
Bubbleioa <1342130847@qq.com>
2023-02-01 23:54:47 +0800

    feat: connect to RPC

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"github.com/808-not-found/tik_duck/cmd/web/rpc"
 	"github.com/808-not-found/tik_duck/cmd/web/service"
 	"github.com/808-not-found/tik_duck/pkg/consts"
 	"github.com/cloudwego/hertz/pkg/app/server"
 )
 
 func main() {
+	rpc.InitRPC()
 	go service.RunMessageServer()
 	h := server.Default(server.WithHostPorts("0.0.0.0:8080"),
 		server.WithMaxRequestBodySize(consts.HTTPMaxBodySize))
