574fdcfdcd3207801f705eb7b8cb6a3c2b07786a
Bubbleioa <1342130847@qq.com>
2023-02-01 23:40:10 +0800

    feat: use etcd handle rpc service

--- pkg/consts/consts.go
+++ pkg/consts/consts.go
@@ -9,7 +9,7 @@ const (
 	MessageTableName    = "message"
 	UserTableName       = "user"
 	VideoTableName      = "video"
-	EtcdAddress         = "Etcd:2379"
+	EtcdAddress         = "127.0.0.1:2379"
 	UserServiceName     = "user"
 	UserUserServiceName = "useruser"
 	UserPlatServiceName = "userplat"
