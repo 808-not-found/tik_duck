17244dc4d95dcc59c3ddd951958eaf21d1dcc12d
Lingsb <3422284835@qq.com>
2023-02-13 14:28:45 +0800

    feat:Add db- like

--- cmd/userplat/dal/db/like.go
+++ cmd/userplat/dal/db/like.go
@@ -1,10 +1,7 @@
 package db
 
 import (
-<<<<<<< HEAD
 	"context"
-=======
->>>>>>> 1c9f1fabb3ba70047ca1f3174baaf9d20ca81f0c
 	"time"
 
 	"github.com/808-not-found/tik_duck/pkg/consts"
@@ -19,7 +16,6 @@ type Like struct {
 	VideoID  int64     `gorm:"column:video_id;default:0;NOT NULL"`
 }
 
-<<<<<<< HEAD
 type Video struct {
 	gorm.Model
 	ID            int64     `gorm:"column:id;primary_key;AUTO_INCREMENT"`
@@ -120,8 +116,3 @@ func GetFavoriteList(ctx context.Context, myID int64) ([]*Video, error) {
 	return res, nil
 
 }
-=======
-func (like *Like) TableName() string {
-	return consts.LikeTableName
-}
->>>>>>> 1c9f1fabb3ba70047ca1f3174baaf9d20ca81f0c
