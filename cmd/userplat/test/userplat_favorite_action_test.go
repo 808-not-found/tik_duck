f6a0982949356db438af7706016f4be6e59d5573
TTaket <575500603@qq.com>
2023-02-21 02:42:38 +0800

    merge: 合并上流分支

--- cmd/userplat/test/userplat_favorite_action_test.go
+++ cmd/userplat/test/userplat_favorite_action_test.go
@@ -2,36 +2,89 @@ package userplatservice_test
 
 import (
 	"context"
-	"reflect"
 	"testing"
 
+	"github.com/808-not-found/tik_duck/cmd/userplat/dal/db"
 	userplatservice "github.com/808-not-found/tik_duck/cmd/userplat/userplatService"
 	"github.com/808-not-found/tik_duck/kitex_gen/userplat"
+	"github.com/808-not-found/tik_duck/pkg/allerrors"
+	"github.com/808-not-found/tik_duck/pkg/jwt"
+	. "github.com/bytedance/mockey"
+	"github.com/stretchr/testify/assert"
 )
 
+// struct FavoriteActionRequest {
+//     1: string Token (go.tag = 'json:"token"')
+//     2: i64 VideoId (go.tag = 'json:"video_id"')
+//     3: i32 ActionType (go.tag = 'json:"action_type"')
+// }
+
+//	struct FavoriteActionResponse {
+//	    1: i32 StatusCode   (go.tag = 'json:"status_code"')        //状态码，0-成功，其他值失败
+//	    2: string StatusMsg (go.tag = 'json:"status_msg"')        // 返回状态描述
+//	}
 func TestUserFavoriteActionService(t *testing.T) {
-	type args struct {
-		ctx context.Context //nolint
-		req *userplat.FavoriteActionRequest
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    *userplat.FavoriteActionResponse
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := userplatservice.UserFavoriteActionService(tt.args.ctx, tt.args.req)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("UserFavoriteActionService() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("UserFavoriteActionService() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	//鉴权失败
+	PatchConvey("TestMockUserplatFavoriteAction_WorryClaim", t, func() {
+		expectstatusCode := int32(2040)
+		var expectStatusMsg *string
+		Mock(jwt.ParseToken).Return(&jwt.MyClaims{}, allerrors.ErrTestnotnil()).Build()
+		req := userplat.FavoriteActionRequest{
+			Token:      "3",
+			VideoId:    999,
+			ActionType: 1,
+		}
+		res, err := userplatservice.UserFavoriteActionService(context.Background(), &req)
+		assert.Equal(t, expectstatusCode, res.StatusCode)
+		assert.Equal(t, expectStatusMsg, res.StatusMsg)
+		assert.Equal(t, err, allerrors.ErrTestnotnil())
+	})
+	//点赞失败
+	PatchConvey("TestMockUserplatFavoriteAction_Worrylike", t, func() {
+		expectstatusCode := int32(2041)
+		var expectStatusMsg *string
+		Mock(jwt.ParseToken).Return(&jwt.MyClaims{}, nil).Build()
+		Mock(db.LikeAction).Return(allerrors.ErrTestnotnil()).Build()
+		req := userplat.FavoriteActionRequest{
+			Token:      "3",
+			VideoId:    999,
+			ActionType: 1,
+		}
+		res, err := userplatservice.UserFavoriteActionService(context.Background(), &req)
+		assert.Equal(t, expectstatusCode, res.StatusCode)
+		assert.Equal(t, expectStatusMsg, res.StatusMsg)
+		assert.Equal(t, err, allerrors.ErrTestnotnil())
+	})
+	//取消点赞失败
+	PatchConvey("TestMockUserplatFavoriteAction_Worryunlike", t, func() {
+		expectstatusCode := int32(2041)
+		var expectStatusMsg *string
+		Mock(jwt.ParseToken).Return(&jwt.MyClaims{}, nil).Build()
+		Mock(db.UnLikeAction).Return(allerrors.ErrTestnotnil()).Build()
+		req := userplat.FavoriteActionRequest{
+			Token:      "3",
+			VideoId:    999,
+			ActionType: 2,
+		}
+		res, err := userplatservice.UserFavoriteActionService(context.Background(), &req)
+		assert.Equal(t, expectstatusCode, res.StatusCode)
+		assert.Equal(t, expectStatusMsg, res.StatusMsg)
+		assert.Equal(t, err, allerrors.ErrTestnotnil())
+	})
+	//点赞成功
+	PatchConvey("TestMockUserplatFavoriteAction_Normal", t, func() {
+		expectstatusCode := int32(0)
+		var expectStatusMsg *string
+		Mock(jwt.ParseToken).Return(&jwt.MyClaims{}, nil).Build()
+		Mock(db.LikeAction).Return(nil).Build()
+		req := userplat.FavoriteActionRequest{
+			Token:      "3",
+			VideoId:    999,
+			ActionType: 1,
+		}
+		res, err := userplatservice.UserFavoriteActionService(context.Background(), &req)
+		assert.Equal(t, expectstatusCode, res.StatusCode)
+		assert.Equal(t, expectStatusMsg, res.StatusMsg)
+		assert.Equal(t, err, nil)
+	})
 }
--- cmd/userplat/userplatService/userplat_comment_action.go
+++ cmd/userplat/userplatService/userplat_comment_action.go
@@ -13,54 +13,37 @@ func UserCommentActionService(
 	ctx context.Context,
 	req *userplat.CommentActionRequest,
 ) (*userplat.CommentActionResponse, error) {
-	var resp userplat.CommentActionResponse
-
-	// 用户鉴权
-	var myID int64
-	if req.Token == "" {
-		myID = 0
-	} else {
-		claims, err := jwt.ParseToken(req.Token)
-		if err != nil {
-			resp.StatusCode = 2034
-			return &resp, nil
-		}
-		myID = claims.ID
+	resp := userplat.CommentActionResponse{
+		StatusCode: 0,
 	}
-	// 获取必要信息
-	// 1.获取登录用户ID
-	// 2.获取当前视频ID
-	// 检查是进行评论还是删除
+	// 用户鉴权
+	claims, err := jwt.ParseToken(req.Token)
+	if err != nil {
+		resp.StatusCode = 2034
+		return &resp, nil
+	}
+	myID := claims.ID
 	vdID := req.VideoId
 	actionType := req.ActionType
 	commentText := req.CommentText
 	commentID := req.CommentId
-	// 检查登录状态
-	// if myID == 0 {
-	// 	resp.StatusCode = 1008
-	// 	return &resp, err
-	// }
-	if actionType == 1 {
-		// 评论,操作数据库：
-		// 返回两个值一条为评论的信息，目前没有处理完这步
 
-		var dbComment *db.Comment
-		// dbComments, err = db.GetFavoriteList(ctx, req.UserId)
+	if actionType == 1 {
+		// 评论
 		dbComment, err := db.CommentAction(ctx, myID, vdID, *commentText)
 		if err != nil {
 			resp.StatusCode = 2035
 			return &resp, err
 		}
-		// 数据库封装
+		// 封装
 		rpcComment, err := pack.Comment(ctx, dbComment, myID)
 		if err != nil {
 			resp.StatusCode = 2036
 			return &resp, err
 		}
 		resp.Comment = rpcComment
-		// return &resp, nil
 	} else {
-		// 取消评论,操作数据库
+		// 取消评论
 		err := db.UnCommentAction(ctx, myID, vdID, *commentID)
 		if err != nil {
 			resp.StatusCode = 2035
