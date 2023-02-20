29909483bd8b1d6c54a00862769c4fe165b099e1
TTaket <575500603@qq.com>
2023-02-20 21:09:26 +0800

    test: user服务测试完成

--- cmd/user/test/user_publish_action_test.go
+++ cmd/user/test/user_publish_action_test.go
@@ -2,36 +2,77 @@ package userservice_test
 
 import (
 	"context"
-	"reflect"
 	"testing"
 
+	"github.com/808-not-found/tik_duck/cmd/user/dal/db"
 	userservice "github.com/808-not-found/tik_duck/cmd/user/userService"
 	"github.com/808-not-found/tik_duck/kitex_gen/user"
+	"github.com/808-not-found/tik_duck/pkg/allerrors"
+	"github.com/808-not-found/tik_duck/pkg/jwt"
+	. "github.com/bytedance/mockey"
+	"github.com/stretchr/testify/assert"
 )
 
+//	struct PublishActionRequest {
+//	    1: string Token (go.tag = 'json:"token"') //用户鉴权token
+//	    2: string FilePath (go.tag = 'json:"file_path"')  // 视频路径
+//	    3: string CoverPath (go.tag = 'json:"cover_path"') // 封面路径
+//	    4: string Title (go.tag = 'json:"title"') //视频标题
+//	}
+//
+//	struct PublishActionResponse {
+//	    1: i32 StatusCode (go.tag = 'json:"status_code"') //状态码，0-成功，其他值-失败
+//	    2: optional string StatusMsg (go.tag = 'json:"status_msg"') //返回状态描述
+//	}
 func TestUserPublishActionService(t *testing.T) {
-	type args struct {
-		ctx context.Context //nolint
-		req *user.PublishActionRequest
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    *user.PublishActionResponse
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := userservice.UserPublishActionService(tt.args.ctx, tt.args.req)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("UserPublishActionService() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("UserPublishActionService() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	//用户鉴权失败
+	PatchConvey("TestMockUserPublishAction_WorryClaims", t, func() {
+		expectstatusCode := int32(1024)
+		expecterr := allerrors.ErrTestnotnil()
+		var expextStatusMsg *string
+		Mock(jwt.ParseToken).Return(&jwt.MyClaims{}, allerrors.ErrTestnotnil()).Build()
+		req := user.PublishActionRequest{
+			Token:     "1926",
+			FilePath:  "public/0.mp4",
+			CoverPath: "public/8.jpg",
+			Title:     "17",
+		}
+		res, err := userservice.UserPublishActionService(context.Background(), &req)
+		assert.Equal(t, expectstatusCode, res.StatusCode)
+		assert.Equal(t, expextStatusMsg, res.StatusMsg)
+		assert.Equal(t, expecterr, err)
+	})
+	//数据写入失败
+	PatchConvey("TestMockUserPublishAction_WorryDb", t, func() {
+		expectstatusCode := int32(1025)
+		expecterr := allerrors.ErrTestnotnil()
+		var expextStatusMsg *string
+		Mock(jwt.ParseToken).Return(&jwt.MyClaims{}, nil).Build()
+		Mock(db.UserPublishAction).Return(allerrors.ErrTestnotnil()).Build()
+		req := user.PublishActionRequest{
+			Token:     "1926",
+			FilePath:  "public/0.mp4",
+			CoverPath: "public/8.jpg",
+			Title:     "17",
+		}
+		res, err := userservice.UserPublishActionService(context.Background(), &req)
+		assert.Equal(t, expectstatusCode, res.StatusCode)
+		assert.Equal(t, expextStatusMsg, res.StatusMsg)
+		assert.Equal(t, expecterr, err)
+	})
+	//正常情况
+	PatchConvey("TestMockUserPublishAction_normal", t, func() {
+		expectstatusCode := int32(0)
+		Mock(jwt.ParseToken).Return(&jwt.MyClaims{}, nil).Build()
+		Mock(db.UserPublishAction).Return(nil).Build()
+		req := user.PublishActionRequest{
+			Token:     "1926",
+			FilePath:  "public/0.mp4",
+			CoverPath: "public/8.jpg",
+			Title:     "17",
+		}
+		res, err := userservice.UserPublishActionService(context.Background(), &req)
+		assert.Equal(t, expectstatusCode, res.StatusCode)
+		assert.Equal(t, err, nil)
+	})
 }
--- cmd/user/test/user_publish_list_test.go
+++ cmd/user/test/user_publish_list_test.go
@@ -2,36 +2,117 @@ package userservice_test
 
 import (
 	"context"
-	"reflect"
 	"testing"
+	"time"
 
+	"github.com/808-not-found/tik_duck/cmd/user/dal/db"
+	"github.com/808-not-found/tik_duck/cmd/user/pack"
 	userservice "github.com/808-not-found/tik_duck/cmd/user/userService"
 	"github.com/808-not-found/tik_duck/kitex_gen/user"
+	"github.com/808-not-found/tik_duck/pkg/allerrors"
+	"github.com/808-not-found/tik_duck/pkg/consts"
+	"github.com/808-not-found/tik_duck/pkg/jwt"
+	. "github.com/bytedance/mockey"
+	"github.com/stretchr/testify/assert"
 )
 
+//	struct PublishListRequest {
+//	   1: i64 UserId (go.tag = 'json:"user_id"') //用户id
+//	   2: string Token (go.tag = 'json:"token"') //用户鉴权token
+//	}
+//
+//	struct PublishListResponse {
+//	   1: i32 StatusCode (go.tag = 'json:"status_code"') //状态码，0-成功，其他值-失败
+//	   2: optional string StatusMsg (go.tag = 'json:"status_msg"') //返回状态描述
+//	   3: list<Video> VideoList (go.tag = 'json:"video_list"') //用户发布的视频列表
+//	}
 func TestUserPublishListService(t *testing.T) {
-	type args struct {
-		ctx context.Context //nolint
-		req *user.PublishListRequest
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    *user.PublishListResponse
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := userservice.UserPublishListService(tt.args.ctx, tt.args.req)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("UserPublishListService() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("UserPublishListService() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	nowTime := time.Now()
+	retVideo := make([]*db.Video, 0)
+	retVideo = append(retVideo, &db.Video{
+		ID: 1, AuthorID: 1, PublishTime: nowTime, FilePath: "public/123.mp4", CoverPath: "public/123.jpg",
+		FavoriteCount: 0, CommentCount: 0,
+		Title: "test",
+	},
+	)
+	expectVideo := make([]*user.Video, 0)
+	expectVideo = append(expectVideo, &user.Video{
+		Id: 1, Author: &user.User{
+			Id:            1,
+			Name:          "蒂萨久",
+			FollowCount:   nil,
+			FollowerCount: nil,
+			IsFollow:      false,
+		},
+		PlayPath:      "http://" + consts.WebServerPublicIP + ":" + consts.StaticPort + "/" + "public/123.mp4",
+		CoverPath:     "public/123.jpg",
+		FavoriteCount: 0, CommentCount: 0,
+		IsFavorite: false, Title: "test"},
+	)
+	// 用户鉴权失败
+	PatchConvey("TestMockUserPublishList_WorryClaims", t, func() {
+		expectstatusCode := int32(1026)
+		var expectStatuMsg *string
+		var expectVideo []*user.Video
+		Mock(jwt.ParseToken).Return(&jwt.MyClaims{}, allerrors.ErrTestnotnil()).Build()
+		req := user.PublishListRequest{
+			UserId: 222,
+			Token:  "777",
+		}
+		res, err := userservice.UserPublishListService(context.Background(), &req)
+		assert.Equal(t, expectstatusCode, res.StatusCode)
+		assert.Equal(t, expectStatuMsg, res.StatusMsg)
+		assert.Equal(t, expectVideo, res.VideoList)
+		assert.Equal(t, err, nil)
+	})
+	// 获取数据失败
+	PatchConvey("TestMockUserPublishList_WorrydbVideo", t, func() {
+		expectstatusCode := int32(1027)
+		var expectStatuMsg *string
+		var expectVideo []*user.Video
+		Mock(jwt.ParseToken).Return(&jwt.MyClaims{}, nil).Build()
+		Mock(db.UserPublishList).Return(retVideo, allerrors.ErrTestnotnil()).Build()
+		req := user.PublishListRequest{
+			UserId: 222,
+			Token:  "777",
+		}
+		res, err := userservice.UserPublishListService(context.Background(), &req)
+		assert.Equal(t, expectstatusCode, res.StatusCode)
+		assert.Equal(t, expectStatuMsg, res.StatusMsg)
+		assert.Equal(t, expectVideo, res.VideoList)
+		assert.Equal(t, err, nil)
+	})
+	// 封装数据失败
+	PatchConvey("TestMockUserPublishList_WorryrpcVideo", t, func() {
+		expectstatusCode := int32(1028)
+		var expectStatuMsg *string
+		var expectVideo []*user.Video
+		Mock(jwt.ParseToken).Return(&jwt.MyClaims{}, nil).Build()
+		Mock(db.UserPublishList).Return(retVideo, nil).Build()
+		Mock(pack.Videos).Return(expectVideo, allerrors.ErrTestnotnil()).Build()
+		req := user.PublishListRequest{
+			UserId: 222,
+			Token:  "777",
+		}
+		res, err := userservice.UserPublishListService(context.Background(), &req)
+		assert.Equal(t, expectstatusCode, res.StatusCode)
+		assert.Equal(t, expectStatuMsg, res.StatusMsg)
+		assert.Equal(t, expectVideo, res.VideoList)
+		assert.Equal(t, err, nil)
+	})
+	// 正常情况
+	PatchConvey("TestMockUserPublishList_normal", t, func() {
+		expectstatusCode := int32(0)
+		Mock(jwt.ParseToken).Return(&jwt.MyClaims{}, nil).Build()
+		Mock(db.UserPublishList).Return(retVideo, nil).Build()
+		Mock(pack.Videos).Return(expectVideo, nil).Build()
+		req := user.PublishListRequest{
+			UserId: 222,
+			Token:  "777",
+		}
+		res, err := userservice.UserPublishListService(context.Background(), &req)
+		assert.Equal(t, expectstatusCode, res.StatusCode)
+		assert.Equal(t, expectVideo, res.VideoList)
+		assert.Equal(t, err, nil)
+	})
 }
