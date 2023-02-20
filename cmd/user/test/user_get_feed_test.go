1d08916d946f2247f074144e2cfb7b905094311b
TTaket <575500603@qq.com>
2023-02-20 11:59:05 +0800

    docs: add note

--- cmd/user/test/user_get_feed_test.go
+++ cmd/user/test/user_get_feed_test.go
@@ -15,6 +15,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+//	struct FeedRequest {
+//	    1: optional i64 LatestTime (go.tag = 'json:"latest_time"') //可选参数，限制返回视频的最新投稿时间戳，精确到秒，不填表示当前时间
+//	    2: optional string Token (go.tag = 'json:"token"') // 可选参数，登录用户设置
+//	}
+//
+//	struct FeedResponse {
+//	    1: i32 StatusCode (go.tag = 'json:"status_code"') //状态码，0-成功，其他值失败
+//	    2: optional string StatusMsg (go.tag = 'json:"status_msg"') //返回状态描述
+//	    3: list<Video> VideoList (go.tag = 'json:"video_list"') //视频列表
+//	    4: optional i64 NextTime (go.tag = 'json:"next_time"') //本次返回的视频中，发布最早的时间，作为下次请求时的latest_time
+//	}
 func TestUserGetFeedService(t *testing.T) {
 	nowTime := time.Now()
 	retVideo := make([]*db.Video, 0)
