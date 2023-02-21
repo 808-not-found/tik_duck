5e79ff2ebcf65a80494d8ff5948c3121d2951725
TTaket <575500603@qq.com>
2023-02-22 01:22:20 +0800

    merge: 合并上流

--- cmd/useruser/benchmark/action_test.go
+++ cmd/useruser/benchmark/action_test.go
@@ -25,177 +25,28 @@ import (
 //	    1: i32 StatusCode (go.tag = 'json:"status_code"')//状态码，0- 成功，其他值-失败
 //	    2: optional string StatusMsg (go.tag = 'json:"status_msg"')//返回状态描述
 //	}
-func TestUserRelationActionService(t *testing.T) {
-	// 构建通用信息
 
-	// 正确情况测试_关注
-	PatchConvey("TestUserRelationActionService_normal", t, func() {
-		//设置期待值
-		expectstatusCode := int32(0)
-		var expectMsg *string
-
-		// 设定mock函数
-		// 这部分主要是设定 被测试函数内部调用的别的函数 修改他们返回的结果
-		Mock(jwt.ParseToken).Return(&jwt.MyClaims{ID: 1}, nil).Build()
-		Mock(db.FollowAction).Return(nil).Build()
-		Mock(db.UnFollowAction).Return(nil).Build()
-
+func BenchmarkUserRelationActionService(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
 		//设置传入参数
-		req := useruser.RelationActionRequest{
-			Token:      "123123",
-			ToUserId:   1,
-			ActionType: 1,
+		req := useruser.UserRequest{
+			Token:  "1231312",
+			UserId: 1231231,
 		}
-
-		//调用函数
-		res, err := uuservice.UserRelationActionService(context.Background(), &req)
-
-		//对比返回值
-		assert.Equal(t, expectstatusCode, res.StatusCode)
-		assert.Equal(t, expectMsg, res.StatusMsg)
-		assert.Equal(t, nil, err)
-	})
-
-	// 正确情况测试_取消关注
-	PatchConvey("TestUserRelationActionService_normal", t, func() {
-		//设置期待值
-		expectstatusCode := int32(0)
-		var expectMsg *string
-
-		// 设定mock函数
-		// 这部分主要是设定 被测试函数内部调用的别的函数 修改他们返回的结果
-		Mock(jwt.ParseToken).Return(&jwt.MyClaims{ID: 1}, nil).Build()
-		Mock(db.FollowAction).Return(nil).Build()
-		Mock(db.UnFollowAction).Return(nil).Build()
-
-		//设置传入参数
-		req := useruser.RelationActionRequest{
-			Token:      "123123",
-			ToUserId:   1,
-			ActionType: 2,
+		_, _, _, _ = uuservice.UserRelationActionService(context.Background(), &req)
+	}
+}
+func BenchmarkUserRelationActionServiceParallel(b *testing.B) {
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			//设置传入参数
+			req := useruser.UserRequest{
+				Token:  "1231312",
+				UserId: 1231231,
+			}
+			_, _, _, _ = uuservice.UserRelationActionService(context.Background(), &req)
 		}
-
-		//调用函数
-		res, err := uuservice.UserRelationActionService(context.Background(), &req)
-
-		//对比返回值
-		assert.Equal(t, expectstatusCode, res.StatusCode)
-		assert.Equal(t, expectMsg, res.StatusMsg)
-		assert.Equal(t, nil, err)
-	})
-
-	// 未登陆
-	PatchConvey("TestUserRelationActionService_WithoutToken", t, func() {
-		//设置期待值
-		expectstatusCode := int32(3002)
-		var expectMsg *string
-
-		// 设定mock函数
-		// 这部分主要是设定 被测试函数内部调用的别的函数 修改他们返回的结果
-		Mock(jwt.ParseToken).Return(&jwt.MyClaims{ID: 0}, nil).Build()
-		Mock(db.FollowAction).Return(nil).Build()
-		Mock(db.UnFollowAction).Return(nil).Build()
-
-		//设置传入参数
-		req := useruser.RelationActionRequest{
-			Token:      "123123",
-			ToUserId:   1,
-			ActionType: 1,
-		}
-
-		//调用函数
-		res, err := uuservice.UserRelationActionService(context.Background(), &req)
-
-		//对比返回值
-		assert.Equal(t, expectstatusCode, res.StatusCode)
-		assert.Equal(t, expectMsg, res.StatusMsg)
-		assert.Equal(t, nil, err)
-	})
-
-	// 验证Token失败
-	PatchConvey("TestUserRelationActionService_WrongToken", t, func() {
-		//设置期待值
-		expectstatusCode := int32(3001)
-		expectErr := allerrors.ErrTestnotnil()
-		var expectMsg *string
-
-		// 设定mock函数
-		// 这部分主要是设定 被测试函数内部调用的别的函数 修改他们返回的结果
-		Mock(jwt.ParseToken).Return(&jwt.MyClaims{ID: 0}, allerrors.ErrTestnotnil()).Build()
-		Mock(db.FollowAction).Return(nil).Build()
-		Mock(db.UnFollowAction).Return(nil).Build()
-
-		//设置传入参数
-		req := useruser.RelationActionRequest{
-			Token:      "123123",
-			ToUserId:   1,
-			ActionType: 1,
-		}
-
-		//调用函数
-		res, err := uuservice.UserRelationActionService(context.Background(), &req)
-
-		//对比返回值
-		assert.Equal(t, expectstatusCode, res.StatusCode)
-		assert.Equal(t, expectMsg, res.StatusMsg)
-		assert.Equal(t, expectErr, err)
-	})
-
-	//FollowAction失败
-	PatchConvey("TestUserRelationActionService_WrongFollowAction", t, func() {
-		//设置期待值
-		expectstatusCode := int32(3003)
-		expectErr := allerrors.ErrTestnotnil()
-		var expectMsg *string
-
-		// 设定mock函数
-		// 这部分主要是设定 被测试函数内部调用的别的函数 修改他们返回的结果
-		Mock(jwt.ParseToken).Return(&jwt.MyClaims{ID: 1}, nil).Build()
-		Mock(db.FollowAction).Return(expectErr).Build()
-		Mock(db.UnFollowAction).Return(nil).Build()
-
-		//设置传入参数
-		req := useruser.RelationActionRequest{
-			Token:      "123123",
-			ToUserId:   1,
-			ActionType: 1,
-		}
-
-		//调用函数
-		res, err := uuservice.UserRelationActionService(context.Background(), &req)
-
-		//对比返回值
-		assert.Equal(t, expectstatusCode, res.StatusCode)
-		assert.Equal(t, expectMsg, res.StatusMsg)
-		assert.Equal(t, expectErr, err)
-	})
-
-	// UnFollowAction失败
-	PatchConvey("TestUserRelationActionService_WrongUnFollowAction", t, func() {
-		//设置期待值
-		expectstatusCode := int32(3004)
-		expectErr := allerrors.ErrTestnotnil()
-		var expectMsg *string
-
-		// 设定mock函数
-		// 这部分主要是设定 被测试函数内部调用的别的函数 修改他们返回的结果
-		Mock(jwt.ParseToken).Return(&jwt.MyClaims{ID: 1}, nil).Build()
-		Mock(db.FollowAction).Return(nil).Build()
-		Mock(db.UnFollowAction).Return(expectErr).Build()
-
-		//设置传入参数
-		req := useruser.RelationActionRequest{
-			Token:      "123123",
-			ToUserId:   1,
-			ActionType: 2,
-		}
-
-		//调用函数
-		res, err := uuservice.UserRelationActionService(context.Background(), &req)
-
-		//对比返回值
-		assert.Equal(t, expectstatusCode, res.StatusCode)
-		assert.Equal(t, expectMsg, res.StatusMsg)
-		assert.Equal(t, expectErr, err)
 	})
 }
